4d28fdf358502fe34a421f64789044aa951d4f7f
Matt Thomas <matt@betweenbrain.com>
2011-04-24 21:32:15 -0400

    Added Helvetica font family. Removed features and change log fieldsets. Added change log link to version check.

--- elements/upgradecheck.php
+++ elements/upgradecheck.php
@@ -34,10 +34,10 @@ class JFormFieldUpgradecheck extends JFormField {
 				
 				//If the current version is out of date, notify the user and provide a download link.
 				if ($version < $str)
-					$message = $message . '<a href="http://joomlaengineering.com" target="_blank">Version 1.6.'.$str.' is now available.</a></label>';
+					$message = $message . '<a href="http://joomlaengineering.com" target="_blank">Version 1.6.'.$str.' is now available.</a><br /><a href="http://joomlaengineering.com/construct-community-1-6-changelog" target="_blank">See what&rsquo;s new</a>.</label>';
 				//If the current version is up to date, notify the user. 	
 				elseif (($version == $str) || ($version > $str))
-					$message = $message . 'There are no updates available at this time.</label>';
+					$message = $message . 'There are no updates available at this time.<br /><a href="http://joomlaengineering.com/construct-community-1-6-changelog" target="_blank">View the change log</a>.</label>';
 				return $message;							
 	  }
 }
