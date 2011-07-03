c57d406aff0ae2f3e6360b2aad39ead5b4e08935
WebMechanic <serradeil@webmechanic.biz>
2011-07-03 02:03:51 +0200

    removed leading whitespace before DOCTYPEs
    while I don't care for IE6, anything preceding the DOCTYPE drops this
    browser emulator into "weired mode".

--- modal.php
+++ modal.php
@@ -14,22 +14,20 @@ $logicFile 		= JPATH_THEMES.'/'.$this->template.'/elements/logic.php';
 if(JFile::exists($logicFile)) {
 	include $logicFile;
 }
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 <head>
   <link rel="stylesheet" href="<?php echo $baseUrl.'templates/'.$this->template; ?>/css/screen.css" type="text/css" media="screen" />
   <link rel="stylesheet" href="<?php echo $baseUrl.'templates/'.$this->template; ?>/css/modal.css" type="text/css" media="screen" />
-<?php	
+<?php
 	if ($customStyleSheet !='-1')
 		echo "\n".'  <link rel="stylesheet" href="templates/'.$this->template.'/css/'.$customStyleSheet.'"  type="text/css" media="screen" />';
 	if ($this->direction == 'rtl')
 		echo "\n".'  <link rel="stylesheet" href="templates/'.$this->template.'/css/rtl.css"  type="text/css" media="screen" />';
-?>  
+?>
 </head>
 
 <body>
-	<jdoc:include type="component" />     
+	<jdoc:include type="component" />
 </body>
 </html>
