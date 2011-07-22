a350291e2d61b79ec5e5ad2aa04c20f42edb76ad
WebMechanic <serradeil@webmechanic.biz>
2011-07-22 04:29:52 +0200

    lang files and some overrides from beez5

---
+++ html/mod_menu/default.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * mod_menu override
+ * @package		Template
+ * @subpackage	HTML
+ * @author		WebMechanic http://webmechanic.biz
+ * @copyright	Copyright (C)2011 WebMechanic. All rights reserved.
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+/* search helper knows about the (enhanced) stop words list
+ * and is misused to clean the alias for use as a class name */
+JLoader::register('SearchHelper', JPATH_ADMINISTRATOR .'/components/com_search/helpers/search.php');
+
+// Note. It is important to remove spaces between elements.
+?>
+
+<ul class="menu <?php echo $class_sfx;?>"<?php
+	$tag = '';
+	if ($params->get('tag_id')!=NULL) {
+		$tag = $params->get('tag_id').'';
+		echo ' id="'.$tag.'"';
+	}
+?>>
+<?php
+foreach ($list as $i => &$item) :
+	// $class = 'li-'.strtolower(metaphone($item->alias)).' '; // ;-)
+
+	$alias = str_replace('-', ' ', $item->alias);
+	SearchHelper::santiseSearchWord($alias, $item->alias);
+	$alias = str_replace(' ', '-', $alias);
+	$class = array($alias);
+
+	if ($item->id == $active_id) {
+		$class[] = 'current ';
+	}
+
+	if (	$item->type == 'alias' &&
+			in_array($item->params->get('aliasoptions'),$path)
+		||	in_array($item->id, $path)) {
+	  $class[] = 'active ';
+	}
+	if ($item->deeper) {
+		$class[] = 'deeper ';
+	}
+
+	if ($item->parent) {
+		$class[] = 'parent ';
+	}
+
+	if (!empty($class)) {
+		$class = ' class="'.implode(' ', $class) .'"';
+	}
+
+	echo '<li id="item-'.$item->id.'"'.$class.'>';
+
+	// Render the menu item.
+	switch ($item->type) :
+		case 'separator':
+		case 'url':
+		case 'component':
+			require JModuleHelper::getLayoutPath('mod_menu', 'default_'.$item->type);
+			break;
+
+		default:
+			require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+			break;
+	endswitch;
+
+	// The next item is deeper.
+	if ($item->deeper) {
+		echo '<ul>';
+	}
+	// The next item is shallower.
+	else if ($item->shallower) {
+		echo '</li>';
+		echo str_repeat('</ul></li>', $item->level_diff);
+	}
+	// The next item is on the same level.
+	else {
+		echo '</li>';
+	}
+endforeach;
+?></ul>
