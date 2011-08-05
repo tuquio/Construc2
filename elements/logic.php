a35abf4d80d209651b276a9eec8d37ce96ca4a63
WebMechanic <serradeil@webmechanic.biz>
2011-08-05 02:58:56 +0200

    CSS cleanup, html5-shim, language rephrasing - template xml: IE fieldset description and experimental google chrome frame option - logic.php: made JLoader::register() work

--- elements/logic.php
+++ elements/logic.php
@@ -3,23 +3,22 @@
  * @package		Templates
  * @subpackage  Helper
  * @author		Joomla Engineering http://joomlaengineering.com
- * @author		WebMechanic http://webmechanic.biz
  * @copyright	Copyright (C) 2010, 2011 Matt Thomas | Joomla Engineering. All rights reserved.
+ * @author		WebMechanic http://webmechanic.biz
+ * @copyright	(C) 2011 WebMechanic
  * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @return JDocumentHtml
- * @uses JSite
- * @uses JFactory
+ * @uses JSite, JFactory, JURI
  */
 
-/** Load the ConstructTemplateHelper Class */
-# JLoader::import($this->template.'.elements.helper', 'templates', 'templates.');
-require_once dirname(__FILE__) . '/helper.php';
+/** Register the ConstructTemplateHelper Class */
+JLoader::register('ConstructTemplateHelper', dirname(__FILE__) . '/helper.php');
 
 /** @var $app JSite To enable use of site configuration */
 $app 					= JFactory::getApplication();
 /** @var $base_url string Get the base URL of the website */
-$base_url 				= JURI::base();
+$base_url 				= JURI::base(true);
 /** @var $tmpl_url string Define relative shortcut for current template directory */
 $tmpl_url 				= $base_url. 'templates/'. $this->template;
 /** @var $url JURI Get the current URL */
@@ -42,7 +41,8 @@ $siteWidthUnit			= 		 $this->params->get('siteWidthUnit');
 $showDiagnostics 		= (bool) $this->params->get('showDiagnostics');
 $loadModal				= (bool) $this->params->get('loadModal');
 $loadMoo 				= (bool) $this->params->get('loadMoo', $loadModal);
-$loadjQuery 			= 		 $this->params->get('loadjQuery');
+$loadJQuery 			= 		 $this->params->get('loadjQuery');
+$loadChromeFrame		= 		 $this->params->get('loadGcf');
 
 /** @var $googleWebFont array Web-Fonts (for BC the 1. param is read using the "old" field name) */
 $googleWebFont = $googleWebFontSize = $googleWebFontTargets = array();
@@ -73,6 +73,38 @@ if ($loadMoo == false) {
 		unset($head['scripts'][$src]);
 	}
 	$this->setHeadData($head);
+}
+
+
+if ($loadJQuery) {
+	$this->addScript($url->getScheme() . '://ajax.googleapis.com/ajax/libs/jquery/'. $loadJQuery .'/jquery.min.js');
+
+	// component views, plugins and modules might use optional
+	// jQuery plugins, but "our" jQuery will come too late for
+	// plugins to bind to jQuery.fn, so we move it up the stack
+	// #TODO handle jQuery version conflicts loaded within components
+	$_head = $this->getHeadData();
+	$_jqjs = preg_grep('#(jquery\.)#', array_keys($_head['scripts']));
+
+	// jQuery CDNs take precendence http://docs.jquery.com/Downloading_jQuery#CDN_Hosted_jQuery
+	$_cdns = array('ajax.googleapis.com', 'ajax.aspnetcdn.com', 'code.jquery.com');
+	foreach ((array) $_jqjs as $i => $_src) {
+		$_host = parse_url($_src, PHP_URL_HOST);
+		if ( in_array($_host, $_cdns) ) {
+			$_jquery = array($_src => $_head['scripts'][$_src]);
+			// nuke old entry
+			unset($_head['scripts'][$_src]);
+			// recombine
+			$_head['scripts'] = $_jquery + $_head['scripts'];
+		}
+	}
+	$this->setHeadData($_head);
+	unset($i, $_src, $_jqjs, $_host, $_jquery);
+
+	// without MooTools we must drop all but core.js
+	if ($loadMoo == true) {
+		$scriptDeclarations[] = 'if (window.jQuery){jQuery.noConflict();}';
+	}
 }
 
 #--------------------------------------------------------------------------#
@@ -140,8 +172,6 @@ if ($columnGroupAlphaCount > 0) {
 	$columnLayout = 'main-beta';
 }
 
-// FB::info($columnGroupCount, 'columns: '.$columnLayout); #HACK FB log filename
-
 #---------------------------- Head Elements --------------------------------#
 
 // Custom tags
@@ -181,7 +211,7 @@ if ($enableSwitcher) {
 for ($i=1; $i<=ConstructTemplateHelper::MAX_WEBFONTS; $i++) {
 	if ($googleWebFont[$i]) {
 		// Fix Google Web Font name for CSS
-		$googleWebFontFamily[$i] = str_replace(array('+',':bold',':italic')," ",$googleWebFont[$i]);
+		$googleWebFontFamily[$i] = str_replace(array('+',':bold',':italic'), ' ', $googleWebFont[$i]);
 		$this->addStyleSheet('//fonts.googleapis.com/css?family='.$googleWebFont[$i].'');
 		$this->addStyleDeclaration(
 			$googleWebFontTargets[$i]
@@ -189,36 +219,6 @@ for ($i=1; $i<=ConstructTemplateHelper::MAX_WEBFONTS; $i++) {
 			.(($googleWebFontSize[$i]>0) ? 'font-size:'.$googleWebFontSize[$i].';' : '')
 			.'}'
 			);
-	}
-}
-
-// JavaScript
-if ($loadMoo == true) {
-	$scriptDeclarations[] = "\tif (window.addEvent){window.addEvent('domready',function(){new SmoothScroll({duration:1200},window);});}";
-}
-if ($loadjQuery) {
-	$this->addScript('//ajax.googleapis.com/ajax/libs/jquery/'. $loadjQuery .'/jquery.min.js');
-
-	$head = $this->getHeadData();
-	$jqjs = preg_grep('#(jquery\.)#', array_keys($head['scripts']));
-/*
- * #FIXME view layouts können jQuery-Plugins dazuladen, die dann
- * jedoch zu früh ausgeführt werden. jQuery selbst wird zu spät geladen...
-	// jQuery Google CDN takes precendence
-	foreach ((array) $jqjs as $i => $url) {
-		if ( false !== strpos($url, 'googleapis.com') ) {
-			$jquery = array($url => $head['scripts'][$url]);
-			// nuke old entry
-			unset($head['scripts'][$url]);
-			// recombine
-			$head['scripts'] = $jquery + $head['scripts'];
-		}
-	}
-	$this->setHeadData($head);
-*/
-	// without MooTools we must drop all but core.js
-	if ($loadMoo == true) {
-		$scriptDeclarations[] = "\tif (window.jQuery){jQuery.noConflict();}";
 	}
 }
 
@@ -233,9 +233,18 @@ if (!$fullWidth) {
 	$this->addStyleDeclaration('#header, #footer {'.$siteWidthType.':'.$siteWidth.$siteWidthUnit.'; margin:0 auto}');
 }
 
-// Internet Explorer Fixes
-# html5 shim
-$this->addCustomTag('<!--[if lt IE 9]><script src="'.$base_url.'media/system/js/html5.js" type="text/javascript"></script><![endif]-->');
+// JavaScript
+if ($loadMoo == true) {
+	$scriptDeclarations[] = 'if (window.addEvent && SmoothScroll){window.addEvent(\'domready\',function(){new SmoothScroll({duration:1200},window);});}';
+}
+
+#----------------------- Internet Explorer Fixes ---------------------------#
+// html5 shim
+$this->addCustomTag('<!--[if lt IE 9]><script src="'.$tmpl_url.'/js/html5.js" type="text/javascript"></script><![endif]-->');
+
+// offer Chrome Frame for IE lt 9
+// $this->addScript('//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js');
+// $this->addCustomTag('<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />');
 
 if ($IECSS3) {
 	$this->addCustomTag(
@@ -270,7 +279,7 @@ $this->addCustomTag(
 
 if ($IE6TransFix) {
 	$this->addScript($tmpl_url.'/js/DD_belatedPNG_0.0.8a-min.js');
-	$scriptDeclarations[] = "\t/* IE6TransFix */ if (DD_belatedPNG.fix) {DD_belatedPNG.fix('". $IE6TransFixTargets ."')}";
+	$scriptDeclarations[] = "\t/* IE6TransFix */ if (DD_belatedPNG) {DD_belatedPNG.fix('". $IE6TransFixTargets ."')}";
 }
 
 // add collected custom script declarations
