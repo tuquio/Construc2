cee1c681953feacd47825ba945adc2651c9cb93c
WebMechanic <serradeil@webmechanic.biz>
2011-11-20 18:49:33 +0100

    Added com_user layout overrides and lang files

--- html/com_users/reset/complete.php
+++ html/com_users/reset/complete.php
@@ -1,41 +1,39 @@
 <?php
 /**
- * @version		$Id: complete.php 22338 2011-11-04 17:24:53Z github_bot $
- * @package		Joomla.Site
- * @subpackage	com_users
- * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- * @since		1.5
+ * Custom Override for com_users.reset.complete
+ *
+ * @package		Templates
+ * @subpackage  Construc2
+ * @author		WebMechanic http://webmechanic.biz
+ * @copyright	(C) 2011 WebMechanic. All rights reserved.
+ * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  */
-
 defined('_JEXEC') or die;
 
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidation');
 ?>
-<div class="reset-complete<?php echo $this->pageclass_sfx?>">
+<div class="account complete">
 	<?php if ($this->params->get('show_page_heading')) : ?>
-	<h1>
-		<?php echo $this->escape($this->params->get('page_heading')); ?>
-	</h1>
+	<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
 	<?php endif; ?>
 
-	<form action="<?php echo JRoute::_('index.php?option=com_users&task=reset.complete'); ?>" method="post" class="form-validate">
-
-		<?php foreach ($this->form->getFieldsets() as $fieldset): ?>
-		<p><?php echo JText::_($fieldset->label); ?></p>		<fieldset>
-			<dl>
-			<?php foreach ($this->form->getFieldset($fieldset->name) as $name => $field): ?>
-				<dt><?php echo $field->label; ?></dt>
-				<dd><?php echo $field->input; ?></dd>
-			<?php endforeach; ?>
+	<form class="form-validate" action="<?php echo JRoute::_('index.php?option=com_users&task=reset.complete'); ?>" method="post">
+<?php foreach ($this->form->getFieldsets() as $fieldset): ?>
+		<p><?php echo JText::_($fieldset->label); ?></p>
+		<fieldset class="complete">
+			<dl class="complete">
+	<?php foreach ($this->form->getFieldset($fieldset->name) as $name => $field): ?>
+				<dt class="<?php echo $field->name; ?>"><?php echo $field->label; ?></dt>
+				<dd class="<?php echo $field->name; ?>"><?php echo $field->input; ?></dd>
+	<?php endforeach; ?>
 			</dl>
 		</fieldset>
-		<?php endforeach; ?>
+<?php endforeach; ?>
 
-		<div>
-			<button type="submit" class="validate"><?php echo JText::_('JSUBMIT'); ?></button>
-			<?php echo JHtml::_('form.token'); ?>
+		<div class="button">
+		<button type="submit" class="validate"><span><?php echo JText::_('JSUBMIT'); ?></span></button>
 		</div>
+	<?php echo JHtml::_('form.token'); ?>
 	</form>
 </div>
