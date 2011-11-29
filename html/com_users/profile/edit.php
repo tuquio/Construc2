f3f4e6642cd5ddb6746d4516543a28dcdb2256a4
WebMechanic <serradeil@webmechanic.biz>
2011-11-29 13:49:43 +0100

    ^ fixed class attribute name in registration/default.php + split profile/edit.php into subtemplates for all user profile fields, however subtemplates are not loaded by default. TODO: add a smart check for the user profile plugin to determine whether or not and if so, which subtemplate is needed.

--- html/com_users/profile/edit.php
+++ html/com_users/profile/edit.php
@@ -1,62 +1,57 @@
 <?php
 /**
- * @version		$Id: edit.php 22357 2011-11-07 08:41:08Z github_bot $
- * @package		Joomla.Site
- * @subpackage	com_users
- * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- * @since		1.6
+ * Custom Override for com_users.profile.edit
+ *
+ * @package		Templates
+ * @subpackage  Construc2
+ * @author		WebMechanic http://webmechanic.biz
+ * @copyright	(C) 2011 WebMechanic. All rights reserved.
+ * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  */
-
 defined('_JEXEC') or die;
 
 JHtml::_('behavior.keepalive');
-JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
-//load user_profile plugin language
-$lang = JFactory::getLanguage();
-$lang->load( 'plg_user_profile', JPATH_ADMINISTRATOR );
 ?>
-<div class="profile-edit<?php echo $this->pageclass_sfx?>">
+<div class="line account profile-edit">
 <?php if ($this->params->get('show_page_heading')) : ?>
 	<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
 <?php endif; ?>
 
-<form id="member-profile" action="<?php echo JRoute::_('index.php?option=com_users&task=profile.save'); ?>" method="post" class="form-validate" enctype="multipart/form-data">
-<?php foreach ($this->form->getFieldsets() as $group => $fieldset):// Iterate through the form fieldsets and display each one.?>
-	<?php $fields = $this->form->getFieldset($group);?>
-	<?php if (count($fields)):?>
-	<fieldset>
-		<?php if (isset($fieldset->label)):// If the fieldset has a label set, display it as the legend.?>
-		<legend><?php echo JText::_($fieldset->label); ?></legend>
-		<?php endif;?>
-		<dl>
-		<?php foreach ($fields as $field):// Iterate through the fields in the set and display them.?>
-			<?php if ($field->hidden):// If the field is hidden, just display the input.?>
-				<?php echo $field->input;?>
-			<?php else:?>
-				<dt>
-					<?php echo $field->label; ?>
-					<?php if (!$field->required && $field->type!='Spacer'): ?>
-					<span class="optional"><?php echo JText::_('COM_USERS_OPTIONAL'); ?></span>
-					<?php endif; ?>
-				</dt>
-				<dd><?php echo $field->input; ?></dd>
-			<?php endif;?>
-		<?php endforeach;?>
-		</dl>
+<form class="form-validate" action="<?php echo JRoute::_('index.php?option=com_users&task=profile.save'); ?>" method="post" enctype="multipart/form-data">
+
+	<fieldset class="profile-edit account">
+	<legend><?php echo JText::_('COM_USERS_PROFILE_ACCOUNT') ?></legend>
+
+	<!-- account details -->
+	<dl class="account">
+		<dt class="name"><?php echo $this->form->getLabel('name'); ?></dt>
+		<dd class="name"><?php echo $this->form->getInput('name'); ?></dd>
+		<dt class="username"><?php echo $this->form->getLabel('username'); ?></dt>
+		<dd class="username"><?php echo $this->form->getInput('username'); ?></dd>
+
+		<dt class="email"><?php echo $this->form->getLabel('email1'); ?></dt>
+		<dd class="email"><?php echo $this->form->getInput('email1'); ?></dd>
+		<dt class="email"><?php echo $this->form->getLabel('email2'); ?></dt>
+		<dd class="email"><?php echo $this->form->getInput('email2'); ?></dd>
+
+		<dt class="password"><?php echo $this->form->getLabel('password1'); ?></dt>
+		<dd class="password"><?php echo $this->form->getInput('password1'); ?></dd>
+		<dt class="password"><?php echo $this->form->getLabel('password2'); ?></dt>
+		<dd class="password"><?php echo $this->form->getInput('password2'); ?></dd>
+	</dl>
 	</fieldset>
-	<?php endif;?>
-<?php endforeach;?>
+<?php
+	/* here's a good place to load the subtemplates based on profile fields */
+?>
+	<div class="line button">
+		<button type="submit" class="validate"><span><?php echo JText::_('JSUBMIT'); ?></span></button>
+		<?php echo JText::_('COM_USERS_OR'); ?>
+		<a href="<?php echo JRoute::_('index.php?option=com_users&view=profile'); ?>" title="<?php echo JText::_('JCANCEL'); ?>"><?php echo JText::_('JCANCEL'); ?></a>
 
-		<div>
-			<button type="submit" class="validate"><span><?php echo JText::_('JSUBMIT'); ?></span></button>
-			<?php echo JText::_('COM_USERS_OR'); ?>
-			<a href="<?php echo JRoute::_(''); ?>" title="<?php echo JText::_('JCANCEL'); ?>"><?php echo JText::_('JCANCEL'); ?></a>
-
-			<input type="hidden" name="option" value="com_users" />
-			<input type="hidden" name="task" value="profile.save" />
-			<?php echo JHtml::_('form.token'); ?>
-		</div>
-	</form>
+		<input type="hidden" name="option" value="com_users" />
+		<input type="hidden" name="task" value="profile.save" />
+	<?php echo JHtml::_('form.token');?>
+	</div>
+</form>
 </div>
