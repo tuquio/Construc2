95cfd60bb29e67df1b00c9a46792700802a9c452
WebMechanic <serradeil@webmechanic.biz>
2011-12-27 00:32:39 +0100

    use SearchHelper to build and samitize <li> alias classes (matches mod_menu)

--- html/com_content/featured/default_links.php
+++ html/com_content/featured/default_links.php
@@ -1,12 +1,22 @@
-<?php defined('_JEXEC') or die; ?>
-<div class="items-more">
+<?php defined('_JEXEC') or die;
+/* REQUIRES the 'SearchHelper' class to be registered in the main default.php Layout */
+?>
+<div class="line items-more">
 <h3><?php echo JText::_('COM_CONTENT_MORE_ARTICLES'); ?></h3>
+<ol class="menu links">
+<?php foreach ($this->link_items as $item) :
+	// create some fancy aliases
+	$alias1 = str_replace('-', ' ', $item->category_alias);
+	SearchHelper::santiseSearchWord($alias1, $item->category_alias);
+	$alias1 = str_replace(' ', '-', $alias1);
 
-<ol class="links">
-<?php foreach ($this->link_items as $li => $item) : ?>
-	<li><a href="<?php
-	echo JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid))
-	?>"><?php echo $item->title ." ($li)" ?></a></li>
+	$alias2 = str_replace('-', ' ', $item->alias);
+	SearchHelper::santiseSearchWord($alias2, $item->alias);
+	$alias2 = str_replace(' ', '-', $alias2);
+?>
+	<li class="li <?php echo $item->category_alias, ' ', $item->alias ?>"><a href="<?php
+	echo JRoute::_(ContentHelperRoute::getArticleRoute($item->id, $item->catid))
+	?>" class="li"><span class="li"><?php echo $item->title ?></span></a></li>
 <?php endforeach; ?>
 </ol>
 </div>
