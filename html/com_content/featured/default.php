26680f7110f9f2bd63015e9e87997e34d8a0aa01
WebMechanic <serradeil@webmechanic.biz>
2011-12-26 22:50:52 +0100

    cleanup and "sync" blog and featured layouts (for they're essentially the same )

--- html/com_content/featured/default.php
+++ html/com_content/featured/default.php
@@ -2,30 +2,28 @@
 // no direct access
 defined('_JEXEC') or die;
 
-JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
-
-// If the page class is defined, add to class as suffix.
-// It will be a separate class if the user starts it with a space
 ?>
 <section class="blog featured">
-<?php if ( $this->params->get('show_page_heading')!=0) : ?>
-	<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
-<?php endif;
+	<header><?php
+if ($this->params->get('show_page_heading')) {
+	echo '<h1 class="page_heading">', $this->escape($this->params->get('page_heading')), '</h1>';
+}
+?>	</header>
+<?php
 
 $leadingcount = $rowcount = $row = 0;
 
 if (!empty($this->lead_items)) { ?>
-<div class="line items-leading"><?php
-	foreach ($this->lead_items as $item)
-	{
-		$leadingcount++;
-
-		$this->item = $item;
+<div class="line items-leading">
+<?php foreach ($this->lead_items as $item) : ?>
+	<div class="leading item-<?php echo $leadingcount, ' ', $item->category_alias, ($item->state == 0 ? ' system-unpublished' : '') ?>">
+<?php
+	$this->item = &$item;
+	echo $this->loadTemplate('item');
 ?>
-<article class="leading-<?php echo $leadingcount .' '. $item->category_alias . ($item->state == 0 ? ' system-unpublished' : ''); ?>">
-<?php echo $this->loadTemplate('item'); ?>
-</article>
-<?php } ?>
+	</div>
+<?php $leadingcount += 1; ?>
+<?php endforeach; ?>
 </div>
 <?php
 }
@@ -35,41 +33,43 @@ $counter    = 0;
 
 if (!empty($this->intro_items))
 {
+	settype($this->columns, 'int');
+	?>
+	<div class="line items-row cols-<?php echo (int) $this->columns ?>">
+	<?php
 	foreach ($this->intro_items as $key => $item)
 	{
-		$key		= ($key - $leadingcount) + 1;
-		$rowcount	= (($key - 1) % (int)$this->columns) + 1;
-		$row 		= $counter / $this->columns ;
-		if ($rowcount == 1)
-		{ ?>
-<div class="line items-row <?php echo 'row-'.$row ?>"><?php
-		}
-
-		$this->item = $item;
+		$key		= (int)($key - $leadingcount) + 1;
+		$rowcount	= ($key - 1) % ($this->columns + 1);
+		$row 		= 1 + ceil($counter / $this->columns);
 ?>
-<article class="unit size1of<?php echo (int) $this->columns .' column-' . $rowcount .' '. $item->category_alias . ($item->state == 0 ? ' system-unpublished"' :''); ?>">
-<?php echo $this->loadTemplate('item'); ?>
-</article>
+	<div class="<?php echo 'row-', $row, (($rowcount & 1) ? ' even' : ' odd'), ' ', $item->category_alias, ($item->state == 0 ? ' system-unpublished':'') ?>">
 <?php
-		$counter++;
-		if (($rowcount == $this->columns) or ($counter == $introcount))
-		{ ?>
-</div><?php
-		}
+	$this->item = &$item;
+	echo $this->loadTemplate('item');
+?>
+	</div>
+<?php
+	$counter += 1;
 	}
+?>
+	</div>
+<?php
 }
 
-if (!empty($this->link_items)) : ?>
-	<div class="line items-more"><?php echo $this->loadTemplate('links'); ?></div>
-<?php endif;
+if (!empty($this->link_items)) {
+	echo '<div class="line items-more">';
+	echo $this->loadTemplate('links');
+	echo '</div>';
+}
 
-if ($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2 && $this->pagination->get('pages.total') > 1)) :
-?>
-	<div class="pagination">
-<?php if ($this->params->def('show_pagination_results', 1)) : ?>
+if ($this->params->def('show_pagination', 2) == 1 || ($this->params->get('show_pagination') == 2 && $this->pagination->get('pages.total') > 1)) { ?>
+	<div class="line pagination">
+	<?php if ($this->params->def('show_pagination_results', 1)) { ?>
 	<p class="counter"><?php echo $this->pagination->getPagesCounter(); ?></p>
-<?php  endif; ?>
-<?php echo $this->pagination->getPagesLinks(); ?>
-	</div>
-<?php endif; ?>
+	<?php  } ?>
+	<?php echo $this->pagination->getPagesLinks(); ?>
+	</div><?php
+}
+?>
 </section>
