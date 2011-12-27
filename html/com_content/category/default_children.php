bce8cf240a95b9647dd300ce3048c8e33646a2b3
WebMechanic <serradeil@webmechanic.biz>
2011-12-27 01:04:24 +0100

    cont. cleanup and "sync" blog and featured layouts

--- html/com_content/category/default_children.php
+++ html/com_content/category/default_children.php
@@ -4,11 +4,13 @@ defined('_JEXEC') or die;
 if (count($this->children[$this->category->id]) > 0) {
 	return;
 }
+$class = ' class="mi first"';
 ?>
 <ul class="menu categories"><?php
 foreach($this->children[$this->category->id] as $id => $child)
 {
-if ($this->params->get('show_empty_categories') || $child->getNumItems(true) || count($child->getChildren())) {
+if ($this->params->get('show_empty_categories') || $child->getNumItems(true) || count($child->getChildren()))
+{
 	if (!isset($this->children[$this->category->id][$id + 1])) {
 		$class = ' class="last"';
 	}
@@ -18,23 +20,26 @@ if ($this->params->get('show_empty_categories') || $child->getNumItems(true) || 
 <?php
 	if ($this->params->get('show_subcat_desc') == 1) {
 		if ($child->description && $this->params->get('show_description')!=0 ) { ?>
-	<div class="category-desc"><?php echo JHtml::_('content.prepare', $child->description); ?></div>
+	<div class="line category-desc"><?php echo JHtml::_('content.prepare', $child->description); ?></div>
 <?php	}
 	}
-	if ($child->getNumItems()==true) { ?>
-	<dl class="article-info">
-	<dt><?php echo JText::_('COM_CONTENT_NUM_ITEMS') ; ?></dt>
-	<dd><?php echo $child->getNumItems(true); ?></dd>
-	</dl><?php
+
+	if ($child->getNumItems()) { ?>
+	<p class="article-info">
+	<strong><?php echo JText::_('COM_CONTENT_NUM_ITEMS') ; ?></strong>
+	<span><?php echo $child->getNumItems(true); ?></span>
+	</p><?php
 	}
 
 	if (count($child->getChildren()) > 0 ) {
 		$this->children[$child->id] = $child->getChildren();
 		$this->category = $child;
 		$this->maxLevel--;
+
 		if ($this->maxLevel != 0) {
 			echo $this->loadTemplate('children');
 		}
+
 		$this->category = $child->getParent();
 		$this->maxLevel++;
 	} ?></li><?php
