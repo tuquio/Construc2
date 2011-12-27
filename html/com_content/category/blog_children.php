c3d4b669cc6bf526e3ca4ddaf9142284c96c45dd
WebMechanic <serradeil@webmechanic.biz>
2011-12-27 01:04:24 +0100

    cont. cleanup and "sync" blog and featured layouts

--- html/com_content/category/blog_children.php
+++ html/com_content/category/blog_children.php
@@ -1,49 +1,48 @@
 <?php
-// no direct access
 defined('_JEXEC') or die;
 
+if (count($this->children[$this->category->id]) > 0) {
+	return;
+}
 $class = ' class="mi first"';
-
-if (count($this->children[$this->category->id]) > 0) : ?>
-<ul class="menu categories">
-<?php foreach($this->children[$this->category->id] as $id => $child) :
-	if ($this->params->get('show_empty_categories') || $child->numitems || count($child->getChildren())) :
-		if (!isset($this->children[$this->category->id][$id + 1])) :
-			$class = ' class="mi last"';
-		endif; ?>
-	<li<?php echo $class; ?>><?php $class = '';
-	?><span class="mi item-title"><a href="<?php
-			echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id))
-			?>"><?php echo $this->escape($child->title); ?></a></span>
-
+?>
+<ul class="menu categories"><?php
+foreach($this->children[$this->category->id] as $id => $child)
+{
+if ($this->params->get('show_empty_categories') || $child->getNumItems(true) || count($child->getChildren()))
+{
+	if (!isset($this->children[$this->category->id][$id + 1])) {
+		$class = ' class="last"';
+	}
+?><li class="mi"><span class="mi item-title"><a href="<?php
+	echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id))
+	?>"><?php echo $this->escape($child->title); ?></a></span>
 <?php
-if ($this->params->get('show_subcat_desc') == 1) :
-	if ($child->description) : ?><div class="category-desc"><?php echo JHtml::_('content.prepare', $child->description); ?></div><?php endif;
-endif;
+	if ($this->params->get('show_subcat_desc') == 1) {
+		if ($child->description && $this->params->get('show_description')!=0 ) { ?>
+	<div class="line category-desc"><?php echo JHtml::_('content.prepare', $child->description); ?></div>
+<?php	}
+	}
 
-if ( $this->params->get('show_cat_num_articles',1)) : ?>
-	<dl class="article-info">
-		<dt><?php echo JText::_('COM_CONTENT_NUM_ITEMS') ; ?></dt>
-		<dd><?php echo $child->getNumItems(true); ?></dd>
-	</dl>
-<?php endif ;
+	if ($child->getNumItems()) { ?>
+	<p class="article-info">
+	<strong><?php echo JText::_('COM_CONTENT_NUM_ITEMS') ; ?></strong>
+	<span><?php echo $child->getNumItems(true); ?></span>
+	</p><?php
+	}
 
-if (count($child->getChildren()) > 0):
-	$this->children[$child->id] = $child->getChildren();
-	$this->category = $child;
-	$this->maxLevel--;
+	if (count($child->getChildren()) > 0 ) {
+		$this->children[$child->id] = $child->getChildren();
+		$this->category = $child;
+		$this->maxLevel--;
 
-	if ($this->maxLevel != 0) :
-		echo $this->loadTemplate('children');
-	endif;
+		if ($this->maxLevel != 0) {
+			echo $this->loadTemplate('children');
+		}
 
-	$this->category = $child->getParent();
-	$this->maxLevel++;
-endif; ?>
-
-	</li><?php
-	endif;
-endforeach; ?>
-</ul>
-<?php
-endif;
+		$this->category = $child->getParent();
+		$this->maxLevel++;
+	} ?></li><?php
+} // if
+} // foreach
+?></ul>
