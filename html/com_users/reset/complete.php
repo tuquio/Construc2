d753d224f6c24c30128a6dbae7694c5ea6415b6f
WebMechanic <serradeil@webmechanic.biz>
2012-01-21 20:02:24 +0100

    refresh and cleanup com_user layout overrides

--- html/com_users/reset/complete.php
+++ html/com_users/reset/complete.php
@@ -13,25 +13,25 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidation');
 ?>
-<div class="account complete">
+<div class="line account complete">
 	<?php if ($this->params->get('show_page_heading')) : ?>
 	<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
 	<?php endif; ?>
 
-	<form class="form-validate" action="<?php echo JRoute::_('index.php?option=com_users&task=reset.complete'); ?>" method="post">
+	<form class="form-validate" id="user-registration" action="<?php echo JRoute::_('index.php?option=com_users&task=reset.complete'); ?>" method="post">
 <?php foreach ($this->form->getFieldsets() as $fieldset): ?>
 		<p><?php echo JText::_($fieldset->label); ?></p>
 		<fieldset class="complete">
-			<dl class="complete">
-	<?php foreach ($this->form->getFieldset($fieldset->name) as $name => $field): ?>
-				<dt class="<?php echo $field->name; ?>"><?php echo $field->label; ?></dt>
-				<dd class="<?php echo $field->name; ?>"><?php echo $field->input; ?></dd>
-	<?php endforeach; ?>
-			</dl>
+		<dl class="complete">
+<?php foreach ($this->form->getFieldset($fieldset->name) as $name => $field): ?>
+			<dt class="<?php echo $field->name; ?>"><?php echo $field->label; ?></dt>
+			<dd class="<?php echo $field->name; ?>"><?php echo $field->input; ?></dd>
+<?php endforeach; ?>
+		</dl>
 		</fieldset>
 <?php endforeach; ?>
 
-		<div class="button">
+		<div class="line button">
 		<button type="submit" class="validate"><span><?php echo JText::_('JSUBMIT'); ?></span></button>
 		</div>
 	<?php echo JHtml::_('form.token'); ?>
--- html/com_users/reset/confirm.php
+++ html/com_users/reset/confirm.php
@@ -13,26 +13,26 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidation');
 ?>
-<div class="account confirm">
+<div class="line account confirm">
 	<?php if ($this->params->get('show_page_heading')) : ?>
 	<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
 	<?php endif; ?>
 
-	<form class="form-validate" action="<?php echo JRoute::_('index.php?option=com_users&task=reset.confirm'); ?>" method="post">
+	<form class="form-validate" id="user-registration" action="<?php echo JRoute::_('index.php?option=com_users&task=reset.confirm'); ?>" method="post">
 <?php foreach ($this->form->getFieldsets() as $fieldset): ?>
 		<p><?php echo JText::_($fieldset->label); ?></p>
-		<fieldset>
-			<dl class="confirm">
-	<?php foreach ($this->form->getFieldset($fieldset->name) as $name => $field): ?>
-				<dt class="<?php echo $field->name; ?>"><?php echo $field->label; ?></dt>
-				<dd class="<?php echo $field->name; ?>"><?php echo $field->input; ?></dd>
-	<?php endforeach; ?>
-			</dl>
+		<fieldset class="confirm">
+		<dl class="confirm">
+<?php foreach ($this->form->getFieldset($fieldset->name) as $name => $field): ?>
+			<dt class="<?php echo $field->name; ?>"><?php echo $field->label; ?></dt>
+			<dd class="<?php echo $field->name; ?>"><?php echo $field->input; ?></dd>
+<?php endforeach; ?>
+		</dl>
 		</fieldset>
 <?php endforeach; ?>
 
-		<div class="button">
-		<button type="submit" class="validate"><?php echo JText::_('JSUBMIT'); ?></button>
+		<div class="line button">
+		<button type="submit" class="validate"><span><?php echo JText::_('JSUBMIT'); ?></span></button>
 		</div>
 	<?php echo JHtml::_('form.token'); ?>
 	</form>
