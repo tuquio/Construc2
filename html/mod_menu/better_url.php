cca8229623a2e88c8043439808b104d03fac1dcd
WebMechanic <serradeil@webmechanic.biz>
2012-02-19 20:31:14 +0100

    mod_menu/better* using either Construc2 new getCssAlias(), and "curly brace" cleanup

--- html/mod_menu/better_url.php
+++ html/mod_menu/better_url.php
@@ -5,7 +5,7 @@
  * @package     Template
  * @subpackage  HTML
  * @author      WebMechanic http://webmechanic.biz
- * @copyright   Copyright (C)2011 WebMechanic. All rights reserved.
+ * @copyright   Copyright (C)2011-2012 WebMechanic. All rights reserved.
  * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -25,15 +25,15 @@ else {
 switch ($item->browserNav) :
 	default:
 	case 0:
-?><a <?php echo $class; ?> href="<?php echo $item->flink; ?>" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+?><a <?php echo $class; ?> href="<?php echo $item->flink ?>" <?php echo $title ?>><?php echo $linktype ?></a><?php
 		break;
 	case 1:
 		// _blank
-?><a <?php echo $class; ?> href="<?php echo $item->flink; ?>" target="_blank" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+?><a <?php echo $class; ?> href="<?php echo $item->flink ?>" target="_blank" <?php echo $title ?>><?php echo $linktype ?></a><?php
 		break;
 	case 2:
 		// window.open
 		$attribs = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,'.$params->get('window_open');
-?><a <?php echo $class; ?> href="<?php echo $item->flink; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $attribs;?>');return false;" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+?><a <?php echo $class ?> href="<?php echo $item->flink ?>" onclick="window.open(this.href,'targetWindow','<?php echo $attribs ?>');return false;" <?php echo $title ?>><?php echo $linktype ?></a><?php
 		break;
 endswitch;
