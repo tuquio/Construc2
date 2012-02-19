c6104900d1bed0bc0de23d3baa1519aaa08eb1d3
WebMechanic <serradeil@webmechanic.biz>
2012-02-19 20:32:29 +0100

    replaced "if endif;" and "foreach endforeach;" with plain good curly braces. to be continued...

--- html/mod_login/links.php
+++ html/mod_login/links.php
@@ -14,9 +14,9 @@ defined('_JEXEC') or die;
 $cuparams = JComponentHelper::getParams('com_users');
 ?>
 <menu class="menu loginmenu">
-<li class="mi reset"><a class="mi" href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>"><span class="mi"><?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></span></a></li>
-<li class="mi remind"><a class="mi" href="<?php echo JRoute::_('index.php?option=com_users&view=remind'); ?>"><span class="mi"><?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_USERNAME'); ?></span></a></li>
-<?php if ($cuparams->get('allowUserRegistration')) :
-?><li class="mi register"><a class="mi" href="<?php echo JRoute::_('index.php?option=com_users&view=registration'); ?>"><span class="mi"><?php echo JText::_('MOD_LOGIN_REGISTER'); ?></span></a></li>
-<?php endif; ?>
+<li class="mi reset"><a class="mi" href="<?php echo JRoute::_('index.php?option=com_users&view=reset') ?>"><span class="mi"><?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD') ?></span></a></li>
+<li class="mi remind"><a class="mi" href="<?php echo JRoute::_('index.php?option=com_users&view=remind') ?>"><span class="mi"><?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_USERNAME') ?></span></a></li>
+<?php if ($cuparams->get('allowUserRegistration')) {
+?><li class="mi register"><a class="mi" href="<?php echo JRoute::_('index.php?option=com_users&view=registration') ?>"><span class="mi"><?php echo JText::_('MOD_LOGIN_REGISTER') ?></span></a></li>
+<?php } ?>
 </menu>
