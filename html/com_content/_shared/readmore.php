c920f615daf12eea75cc82fc5a35d251d8f65ab5
WebMechanic <serradeil@webmechanic.biz>
2012-02-27 00:54:39 +0100

    removed readmore.php, part of ContentLayoutHelper

--- html/com_content/_shared/readmore.php
+++
@@ -1,9 +0,0 @@
-<?php defined('_JEXEC') or die;
-/**
- *
- * @package     Templates
- * @subpackage  Construc2
- * @author      WebMechanic http://webmechanic.biz
- * @copyright   (C) 2011 WebMechanic
- * @license     GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
- */
