10154b84b76db5d8945f951d784708f4ccf59bc5
WebMechanic <serradeil@webmechanic.biz>
2012-02-27 00:12:30 +0100

    rewrite error.php, removed unrelated code from ajax.php (WIP)

--- error.php
+++ error.php
@@ -1,63 +1,102 @@
-<?php
+<?php defined('_JEXEC') or die;
 /**
- * @version		$Id: error.php 21322 2011-05-11 01:10:29Z dextercowley $
- * @package		Joomla.Site
- * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * Main Error Page entry script.
+ *
+ * @package     Templates
+ * @subpackage  Construc2
+ * @author      WebMechanic http://webmechanic.biz
+ * @copyright   (C) 2011 WebMechanic
+ * @license     GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  */
+// 4 l10n stuff
+$doc = JFactory::getDocument();
+list($lang, $region) = explode('-', $doc->language);
 
-defined('_JEXEC') or die;
+$doc_title = $doc->getTitle();
+
 if (!isset($this->error)) {
 	$this->error = JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 	$this->debug = false;
+	if ($doc_title == $this->title) {
+		$this->title = JText::_('JERROR_AN_ERROR_HAS_OCCURRED');
+	}
 }
-//get language and direction
-$doc = JFactory::getDocument();
-$this->language = $doc->language;
-$this->direction = $doc->direction;
+
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<html lang="<?php echo $lang ?>" dir="<?php echo $this->direction ?>">
 <head>
-	<title><?php echo $this->error->getCode(); ?> - <?php echo $this->title; ?></title>
-	<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/system/css/error.css" type="text/css" />
-	<?php if ($this->direction == 'rtl') : ?>
-	<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/system/css/error_rtl.css" type="text/css" />
-	<?php endif; ?>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title><?php echo $doc_title ?> - <?php echo $this->title ?></title>
+<meta name="language" content="<?php echo $lang ?>">
+<meta name="generator" content="Construc2">
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/construc2/css/core/base.css" type="text/css">
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/construc2/css/core/oocss.css" type="text/css">
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/construc2/css/core/screen.css" type="text/css">
 </head>
 <body>
-	<div class="error">
-		<div id="outline">
-		<div id="errorboxoutline">
-			<div id="errorboxheader"><?php echo $this->error->getCode(); ?> - <?php echo $this->error->getMessage(); ?></div>
-			<div id="errorboxbody">
-			<p><strong><?php echo JText::_('JERROR_LAYOUT_NOT_ABLE_TO_VISIT'); ?></strong></p>
-				<ol>
-					<li><?php echo JText::_('JERROR_LAYOUT_AN_OUT_OF_DATE_BOOKMARK_FAVOURITE'); ?></li>
-					<li><?php echo JText::_('JERROR_LAYOUT_SEARCH_ENGINE_OUT_OF_DATE_LISTING'); ?></li>
-					<li><?php echo JText::_('JERROR_LAYOUT_MIS_TYPED_ADDRESS'); ?></li>
-					<li><?php echo JText::_('JERROR_LAYOUT_YOU_HAVE_NO_ACCESS_TO_THIS_PAGE'); ?></li>
-					<li><?php echo JText::_('JERROR_LAYOUT_REQUESTED_RESOURCE_WAS_NOT_FOUND'); ?></li>
-					<li><?php echo JText::_('JERROR_LAYOUT_ERROR_HAS_OCCURRED_WHILE_PROCESSING_YOUR_REQUEST'); ?></li>
-				</ol>
-			<p><strong><?php echo JText::_('JERROR_LAYOUT_PLEASE_TRY_ONE_OF_THE_FOLLOWING_PAGES'); ?></strong></p>
-
-				<ul>
-					<li><a href="<?php echo $this->baseurl; ?>/index.php" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE'); ?>"><?php echo JText::_('JERROR_LAYOUT_HOME_PAGE'); ?></a></li>
-				</ul>
-
-			<p><?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?>.</p>
-			<div id="techinfo">
-			<p><?php echo $this->error->getMessage(); ?></p>
-			<p>
-<?php if ($this->debug || defined('DEVELOPER_MACHINE')) :
-	echo $this->renderBacktrace();
-endif; ?>
-			</p>
-			</div>
-			</div>
+<div class="line error">
+	<div class="mod" id="errorboxoutline">
+		<div class="hd" id="errorboxheader">
+		<h2 class="H3"><q><?php echo $doc_title ?></q>: <?php echo $this->title ?></h2>
 		</div>
+		<div class="bd" id="errorboxbody">
+			<p><?php echo JText::_('JERROR_LAYOUT_NOT_ABLE_TO_VISIT') ?></p>
+			<ol>
+<?php
+	$reasons = array();
+	switch ($this->error->getCode())
+	{
+		case 500:
+			$reasons[] = 'JERROR_LAYOUT_ERROR_HAS_OCCURRED_WHILE_PROCESSING_YOUR_REQUEST';
+			$reasons[] = 'JERROR_LAYOUT_REQUESTED_RESOURCE_WAS_NOT_FOUND';
+			break;
+
+		case 404:
+			$reasons[] = 'JERROR_LAYOUT_SEARCH_ENGINE_OUT_OF_DATE_LISTING';
+			$reasons[] = 'JERROR_LAYOUT_AN_OUT_OF_DATE_BOOKMARK_FAVOURITE';
+			$reasons[] = 'JERROR_LAYOUT_MIS_TYPED_ADDRESS';
+			$reasons[] = 'JERROR_LAYOUT_REQUESTED_RESOURCE_WAS_NOT_FOUND';
+			break;
+
+		case 403:
+			$reasons[] = 'JERROR_LAYOUT_YOU_HAVE_NO_ACCESS_TO_THIS_PAGE';
+			$reasons[] = 'JERROR_TABLE_BIND_FAILED';
+			break;
+
+		case 401:
+			$reasons[] = 'JERROR_LOGIN_DENIED';
+			$reasons[] = 'JERROR_NOLOGIN_BLOCKED';
+			break;
+		default:
+	}
+
+	foreach ($reasons as $reason) {
+		echo '<li>'. JText::_($reason) . '</li>';
+	}
+?>
+			</ol>
+
+<?php if (JComponentHelper::isEnabled('com_search')) { ?>
+			<p><strong><?php echo JText::_('JERROR_LAYOUT_SEARCH') ?></strong></p>
+			<ul>
+				<li><a href="<?php echo JRoute::_($this->baseurl . '/index.php?option=com_search') ?>" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE') ?>"><?php echo JText::_('JERROR_LAYOUT_SEARCH_PAGE') ?></a></li>
+				<li><a href="<?php echo JRoute::_($this->baseurl .'/') ?>" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE') ?>"><?php echo JText::_('JERROR_LAYOUT_HOME_PAGE') ?></a></li>
+			</ul>
+<?php } else {?>
+			<p><strong><?php echo JText::_('JERROR_LAYOUT_PLEASE_TRY_ONE_OF_THE_FOLLOWING_PAGES') ?></strong></p>
+			<ul>
+				<li><a href="<?php echo JRoute::_($this->baseurl .'/') ?>" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE') ?>"><?php echo JText::_('JERROR_LAYOUT_HOME_PAGE') ?></a></li>
+			</ul>
+<?php } ?>
+
+			<p><?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR') ?></p>
+		</div>
+
+		<div class="ft" id="techinfo">
+			<p class="firm"><?php echo $this->error->getMessage() ?></p>
 		</div>
 	</div>
+</div>
 </body>
 </html>
