614ee2a6e3ab68ef4e574a455c9f4ce2ac0f85a1
WebMechanic <serradeil@webmechanic.biz>
2012-02-27 02:48:04 +0100

    Padawan.

--- themes/padawan.php
+++ themes/padawan.php
@@ -1,16 +1,44 @@
 <?php return; ?>
-; Layout configuration
-; Usage:
-; somename=category
-; somename=article
+; -----
+; Theme configuration
+;
+name="Padawan"
+author="WebMechanic.biz"
+version="1.0.0"
+copyright="(c)2011 WebMechanic.biz"
+
+; -----
+; Page level layouts:
+; unique_name=category|article|blog
 ; 	~ layouts/category.php		used for all category views
 ; 	~ layouts/article.php		used for all article views
 ; 	~ layouts/blog.php			used for all blog views
-
+;
+; Item level layouts
 ; hitchhiker=42,article			used for article with ID 42
 ; legacy=62,category			used for category with ID 62
 
 [layouts]
 featured=featured
 category=category
-karriere=62,article
+faq=62,article
+
+; -----
+; Replacing default switches
+; Values are list labels found in 'xx-XX.override.ini'
+[styleswitcher]
+diagnostic=TPL_CONSTRUC2_STYLE_DIAGNOSTIC_MODE
+
+; Color schemes
+.default="Padawan Light"
+contrast="High Contrast"
+darker="Padawan Dark"
+
+; -----
+; Font scaling
+[fontscaler]
+larger="Huge Font"
+large="Larger Font"
+.default="Default Font"
+small="Small Font"
+smaller="Tiny Font"
