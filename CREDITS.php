c7879fd01f24966e046bfb9460e614b4fe04f614
WebMechanic <serradeil@webmechanic.biz>
2012-03-04 20:55:08 +0100

    prepping 1.9.8

--- CREDITS.php
+++ CREDITS.php
@@ -3,27 +3,22 @@
  * @package		Templates
  * @subpackage  Construc2
  * @author		WebMechanic http://webmechanic.biz
- * @copyright	(C) 2011 WebMechanic
+ * @copyright	(C)2011-2012 WebMechanic
  * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  */
 ?><pre>
-"Construc2" (codename) is a HTML5 Template for Joomla 1.7/2.5 and carries several
-ideas inspired by "Construct Community Template Framework for Joomla! 1.6".
+Construc2 is a HTML5 Template for Joomla 2.5 and carries several ideas 
+inspired by "Construct Community Template Framework for Joomla! 1.6".
 It has since been heavily refactored after being forked on gihub.com on
-June 20th, 2011. See README for the many changes.
+June 20th, 2011. See README for changes and features.
 
-"Construc2" includes the OOCSS Framework by Nicole 'stubornella' Sullivan,
-being the most lightweight, yet flexible and "done right" CSS library around
-(the other "done right" framework would be YAML, but it's too heavy lifting for
-my taste.)
-All overrides incl. module Chromes have been adopted to utilize OOCSS to reduce
-the amount of style rules needed.
-
-Component overrides are based off Beez5.
+Construc2 includes OOCSS by Nicole 'stubornella' Sullivan, being the most 
+lightweight, yet flexible and "done right" CSS library around (the other 
+"done right" framework is YAML, but a too heavy lifting for my taste.)
 
 Special thanks to marineart.de and branz-consulting.de for allowing the use of
-several layout and styling techniques "to challenge" their needs for J!1.5 back
-in 2007/2008, and built into their websites: (in no particular order)
+several layout and styling techniques "to challenge" their then needs in J!1.5 
+back in 2007/2008, and built into their websites: (in no particular order)
  - include static HTML "snippet files" Module
  - alternative Homepage layout override
  - dynamic menu item alias as a more powerful replacement for "pageclass_sfx"
@@ -40,20 +35,12 @@ resources that helped to shape it.
 If you feel that you should be included in this list, we offer you our sincerest
 apologies for omitting you and ask that you contact us at joomla@webmechanic.biz
 
+Beez5 layout overrides by Angie Radtke.
+
 "Construct Community Template Framework" for the initial idea and concepts.
 Author:    Joomla Engineering http://joomlaengineering.com
 Copyright: (C) 2010, 2011 Matt Thomas, Joomla Engineering. All rights reserved.
 License:   GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
-
-CSSTidy - CSS Parser and Optimiser PHP class by Florian Schmitz
-http://csstidy.sourceforge.net/
-
-Setting body-width toggle technique from the "JA Purity" Template in Joomla 1.5
-http://www.joomlart.com/
-
-Waseem Sadiq for the Blank Skeleton Template, available at
-http://www.bulletprooftemplates.com/.
-It is a great learning tool and has helped many.
 
 Object Oriented CSS by Nicole 'stubornella' Sullivan
 http://oocss.org - https://github.com/stubbornella/oocss
