097c84dfbd35abcdc47bb95e3c6fb73be23abf9d
WebMechanic <serradeil@webmechanic.biz>
2012-03-05 00:24:11 +0100

    Padawan, cleanup, IE fixes, theme scripts
    + Modernizer for IE

--- themes/padawan.php
+++ themes/padawan.php
@@ -8,13 +8,30 @@ version="1.0.0"
 copyright="(c)2011 WebMechanic.biz"
 
 ; -----
+; All time scripts if SSI powered .scripts don't work for you
+; Filenames are relative to the ./themes folder
+; keyname=N,filename.js
+;   N	meaning
+;   0	disabled
+;   1	all browsers
+;  2-5	reserved
+;	4	all IE
+;   6	IE 6 only
+;   7	IE 7 only
+;   8	IE 8 only
+;   9	IE 9 only
+
+[scripts]
+modernizr=4,modernizr.95863.js
+
+; -----
 ; Page level layouts:
 ; unique_name=category|article|blog
 ; 	~ layouts/category.php		used for all category views
 ; 	~ layouts/article.php		used for all article views
 ; 	~ layouts/blog.php			used for all blog views
 ;
-; Item level layouts
+; Item level layouts:
 ; hitchhiker=42,article			used for article with ID 42
 ; legacy=62,category			used for category with ID 62
 
