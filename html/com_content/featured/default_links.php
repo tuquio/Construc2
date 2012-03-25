0e47f994caa029abb2ed8969f02d36001429c495
WebMechanic <serradeil@webmechanic.biz>
2012-03-25 23:38:54 +0200

    featured/default_links.php, wrong H2 nesting, add .H3 class

--- html/com_content/featured/default_links.php
+++ html/com_content/featured/default_links.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die;
 /* REQUIRES the 'ContentLayoutHelper' class to be registered in the main blog.php Layout */
 ?>
 	<aside id="more" class="items-more">
-		<header><h3><?php echo JText::_('COM_CONTENT_MORE_ARTICLES') ?></h3></header>
+		<header><h2 class="H3"><?php echo JText::_('COM_CONTENT_MORE_ARTICLES') ?></h2></header>
 		<ol class="menu links"><?php
 foreach ($this->link_items as $item)
 {
