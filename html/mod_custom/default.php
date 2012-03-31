2ada3ae5ce1c94ec74b851d2988645e2d9cda427
WebMechanic <serradeil@webmechanic.biz>
2012-04-01 01:25:45 +0200

    mod_custom Housekeeping Overrides

--- html/mod_custom/default.php
+++ html/mod_custom/default.php
@@ -1,14 +1,21 @@
 <?php defined('_JEXEC') or die;
+/**
+ * @package     Template
+ * @subpackage  Overrides
+ * @author		WebMechanic http://webmechanic.biz
+ * @copyright	(C) 2011-2012 WebMechanic. All rights reserved.
+ * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 $attr = array('id="custom-'. $module->id .'"');
 $cls  = array('custom');
-if (!empty($moduleclass_sfx)) $css[] = $moduleclass_sfx;
+if (!empty($moduleclass_sfx)) $cls[] = $moduleclass_sfx;
 
 if ($params->get('backgroundimage')) {
 	$cls[]  = 'has-image';
 	$attr[] = 'style="background-image:url('. $params->get('backgroundimage') .')"';
 }
-$attr[] = implode(' ', $cls);
+$attr[] = ' class="'.implode(' ', $cls) .'"';
 
 if (empty($module->content)) {
 	$module->content = '&nbsp;';
