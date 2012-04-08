4f782343d8e920c7316f3dfa3aecee54c65dc352
WebMechanic <serradeil@webmechanic.biz>
2012-04-09 01:59:57 +0200

    missing index.php

--- layouts/index.php
+++ layouts/index.php
@@ -23,8 +23,8 @@
  * @var bool $enableSwitcher
  */
 !defined('WMPATH_LAYOUTS') && define('WMPATH_LAYOUTS', dirname(__FILE__));
-
 ?>
+
 <html lang="<?php echo $this->language ?>" dir="<?php echo $this->direction ?>" <?php echo $cache_manifest ?>>
 <head>
 <jdoc:include type="head" />
@@ -32,56 +32,52 @@
 <body class="<?php echo $columnLayout ?>">
 <?php ConstructTemplateHelper::msieSwatter(); ?>
 	<div id="page-top" class="line <?php echo $templateHelper->getPageAlias(true) ?>">
-		<div id="page-head" class="line page-head">
-<?php if ($headerAboveCount[0]) : include WMPATH_LAYOUTS. '/mod_header_above.php'; endif; ?>
+		<div id="page-head" class="line page-head"><div class="head-inner">
+<?php if ($headerAboveCount[0]) { include WMPATH_LAYOUTS. '/mod_header_above.php'; } ?>
 
 		<h1 id="logo"><a class="site-link" href="<?php echo $this->baseurl ?>/"><span class="site-name"><?php echo $app->getCfg('sitename');?></span></a></h1>
 <?php if ($this->countModules('header')) : $templateHelper->renderModules('header', 'raw'); endif; ?>
 
-		<?php if ($enableSwitcher) : ?>
-		<ul id="styleswitcher" class="menu style-switcher">
-			<li class="mi switcher wireframe"  data-style="wireframe"><?php echo JText::_('TPL_CONSTRUC2_STYLE_WIREFRAME') ?></li>
-			<li class="mi switcher diagnostic" data-style="diagnostic"><?php echo JText::_('TPL_CONSTRUC2_STYLE_DIAGNOSTIC_MODE') ?></li>
-			<li class="mi switcher normal"     data-style="normal"><?php echo JText::_('TPL_CONSTRUC2_STYLE_NORMAL_MODE') ?></li>
-		</ul>
-		<?php endif; ?>
+		<?php if (count($enableSwitcher)) { ?>
+		<ul id="styleswitcher" class="menu style-switcher"><!-- populated by styleswitcher.js --></ul>
+		<?php } ?>
 
-		</div><!-- .page-head -->
+		</div></div><!-- #page-head -->
 
 		<div id="body-container" class="line <?php echo $templateHelper->getPageAlias() ?>">
-<?php if ($headerBelowCount[0]) : include WMPATH_LAYOUTS. '/mod_header_below.php'; endif; ?>
+<?php if ($headerBelowCount[0]) { include WMPATH_LAYOUTS. '/mod_header_below.php'; } ?>
 
 		<?php if ($this->countModules('nav')) : ?>
 
 			<nav id="mainnavi" class="line mainnavi">
 			<?php $templateHelper->renderModules('nav'); ?>
-			</nav><!-- #mainnavi .mainnavi-->
+			</nav><!-- #mainnavi .mainnavi -->
 		<?php endif; ?>
 
-<?php if ($this->countModules('breadcrumbs')) : $templateHelper->renderModules('breadcrumbs'); endif; ?>
+<?php if ($this->countModules('breadcrumbs')) { $templateHelper->renderModules('breadcrumbs'); } ?>
 
 			<div class="line content-container">
-<?php if ($navBelowCount[0]) : include WMPATH_LAYOUTS. '/mod_nav_below.php'; endif; ?>
+<?php if ($navBelowCount[0]) { include WMPATH_LAYOUTS. '/mod_nav_below.php'; } ?>
 
 				<div id="content" class="line content-main">
 
-<?php if ($this->getBuffer('message')) : ?><jdoc:include type="message" /><?php endif; ?>
+<?php if ($this->getBuffer('message')) { ?><jdoc:include type="message" /><?php } ?>
 
-<?php if ($contentAboveCount[0]) :	include WMPATH_LAYOUTS. '/mod_content_above.php'; endif; ?>
+<?php if ($contentAboveCount[0]) { include WMPATH_LAYOUTS. '/mod_content_above.php'; } ?>
 
 	<section class="line component">
 	<jdoc:include type="component" />
-<?php if ($contentBelowCount[0]) : include WMPATH_LAYOUTS. '/mod_content_below.php'; endif; ?>
+<?php if ($contentBelowCount[0]) { include WMPATH_LAYOUTS. '/mod_content_below.php'; } ?>
 
 	</section>
 
-<?php if ($columnGroupAlphaCount > 0) : include WMPATH_LAYOUTS. '/mod_column_group_alpha.php'; endif; ?>
+<?php if ($columnGroupAlphaCount > 0) { include WMPATH_LAYOUTS. '/mod_column_group_alpha.php'; } ?>
 
-				</div><!-- end content-main -->
+				</div><!-- #content -->
 
-<?php if ($columnGroupBetaCount > 0) : include WMPATH_LAYOUTS. '/mod_column_group_beta.php'; endif; ?>
+<?php if ($columnGroupBetaCount > 0) { include WMPATH_LAYOUTS. '/mod_column_group_beta.php'; } ?>
 
-<?php if ($footerAboveCount[0]) : include WMPATH_LAYOUTS. '/mod_footer_above.php'; endif; ?>
+<?php if ($footerAboveCount[0]) { include WMPATH_LAYOUTS. '/mod_footer_above.php'; } ?>
 
 			</div><!-- .content-container -->
 		</div><!-- #body-container -->
@@ -89,18 +85,19 @@
 
 	<footer id="page-foot" class="line page-foot"><div class="foot-inner">
 <?php /* echo JText::_('TPL_CONSTRUC2_JUMP_BACK_TO_TOP') */ ?>
-<?php if ($this->countModules('syndicate')) : ?>
+<?php if ($this->countModules('syndicate')) { ?>
 
 		<div class="mod syndicate">
-	<?php $templateHelper->renderModules('syndicate'); ?>
+<?php $templateHelper->renderModules('syndicate'); ?>
+
 		</div><!-- .syndicate -->
-<?php endif; ?>
+<?php } ?>
 <?php $templateHelper->renderModules('footer', 'raw'); ?>
 
-	</div></footer><!-- end footer -->
+	</div></footer><!-- #page-foot -->
 <?php
-if ($this->countModules('debug')) : $templateHelper->renderModules('debug', 'raw'); endif;
-if ($this->countModules('analytics')) : $templateHelper->renderModules('analytics', 'raw'); endif;
+if ($this->countModules('debug')) { $templateHelper->renderModules('debug', 'raw'); }
+if ($this->countModules('analytics')) { $templateHelper->renderModules('analytics', 'raw'); }
 
 ConstructTemplateHelper::msieSwatter();
 ?>
