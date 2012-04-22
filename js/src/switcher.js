061882b344d7c3d7a842456784bcc3ed7efdc1b9
WebMechanic <serradeil@webmechanic.biz>
2012-04-22 23:25:29 +0200

    (WIP) switcher.html + switcher.js "NG"

--- js/src/switcher.js
+++ js/src/switcher.js
@@ -1,99 +1,198 @@
-// http://www.alistapart.com/articles/relafont
-// http://www.alistapart.com/articles/alternate
-// brought on par with 2012 practice by WebMechanic.biz
+/*!
+ * StyleSwitcher - an ALA compliant replacement.
+ * @version   0.1.0
+ * @copyright (c)2012 WebMechanic. Some rights reserved.
+ * @licence   CC BY-NC 2.0/de
+ */
+;(function (W, D, undefined) {
+	"use strict";
+	var Switcher = {
+		// list container element
+		elt: null,
+		// list of style sheets
+		styles: {'_default':'normal'},
+		// CSS files location
+		baseUrl: D.location.pathname.split('/').slice(0, -1).join('/'),
+		// default list container id, can be set with StyleSwitcher.list
+		LIST: 'styleswitcher',
+		ITEM_CLASS: 'mi switcher {name}',
+		// storage key name
+		KEY_NAME: 'style',
+		// properties to from from .styles
+		exclude: /(default|href)/i,
 
-(function (window, document, undefined) {
-	function toggleStylesheet(evt) {
-		evt = evt || window.event;
-	}
+		/*
+		* initialize `elt_id` as a container for the Switcher.
+		* look for 'data-styles', then 'LI', and return if neither exists.
+		*
+		* @param	elt_id	String the element id
+		* @param	D		The document object
+		*/
+		init: function(elt_id) {
+			this.elt = D.getElementById(this.LIST);
+			this.elt.style.display = 'none';
 
-	function setActiveStyleSheet(title) {
-		var i, a;
-		console.log('switcher.js', 'setActiveStyleSheet', title);
-		for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
-			if (parseInt(a.getAttribute("rel").indexOf("style"), 10) !== -1 && a.getAttribute("title")) {
-				a.disabled = (a.getAttribute("title") === title);
+			this.readStyles()
+				.restore();
+
+			this.elt.style.display = '';
+
+			if (this.elt.addEventListener) {
+				this.elt.addEventListener('click', this.set, false);
+			} else if (this.elt.attachEvent) {
+				this.elt.attachEvent('onclick', this.set);
 			}
-		}
-	}
+		},
 
-	function getActiveStyleSheet() {
-		var i, a;
-		for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
-			if (parseInt(a.getAttribute("rel").indexOf("style"), 10) !== -1 && a.getAttribute("title") && !a.disabled) {
-				return a.getAttribute("title");
+		/* list item click handler
+		* @param  evt  MouseEvent
+		* @return bool
+		*/
+		set : function(evt) {
+			evt = evt || window.event;
+			evt.stopPropagation();
+			evt.cancelBubble = true;
+			var li = evt.srcElement||evt.target;
+console.log('set', li.dataset, li);
+
+			return false;
+		},
+
+		/* page load event handler
+		* @param  evt  Event
+		* @return boolean
+		*/
+		load: function(evt) {
+			if ('StyleSwitcher' in W) {
+				Switcher.LIST = W.StyleSwitcher.list || Switcher.LIST;
 			}
-		}
-		return null;
-	}
+			Switcher.init.call(Switcher);
+			return true;
+		},
 
-	function getPreferredStyleSheet() {
-		var i, a;
-		for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
-			if (parseInt(a.getAttribute("rel").indexOf("style"), 10) !== -1 && parseInt(a.getAttribute("rel").indexOf("alt"), 10) === -1 && a.getAttribute("title")) {
-				return a.getAttribute("title");
+		/* page unload event handler
+		* @param  evt  Event
+		* @return boolean
+		*/
+		unload: function(evt) {
+			// stores the current style name
+console.log('unload', this);
+			evt = evt||window.event;
+			return true;
+		},
+
+		/* apply the chosen stylesheet and disable the "old" if there is one
+		* @return Switcher
+		*/
+		apply: function() {
+console.log('apply', this, D);
+			return this;
+		},
+
+		/* saves the name of the selected style
+		* @return Switcher
+		*/
+		store: function () {
+console.log('store', this);
+			return this;
+		},
+
+		/* retrieves the name of the last used style
+		* @return Switcher
+		*/
+		restore: function () {
+console.log('restore', this);
+			// read cookie || storage
+
+			// show or build list items
+
+			return this;
+		},
+
+		/* find style names from:
+		* - "data-styles" attribute of list container
+		* - OR existing LI elements in list container
+		* @return Switcher
+		*/
+		readStyles: function() {
+			var data = this.elt.getAttribute('data-styles');
+			if (data) {
+				this.styles = JSON.parse(data);
+				return this.createList();
 			}
-		}
-		return null;
-	}
+			return this.readItems();
+		},
 
-	function createCookie(name, value, days) {
-		var date, expires = "";
-		if (days) {
-			date = new Date();
-			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-			expires = "; expires=" + date.toGMTString();
-		}
-		document.cookie = name + "=" + value + expires + "; path=/";
-	}
-
-	function readCookie(name) {
-		var nameEQ = name + "=", ca = document.cookie.split(';');
-		for ( var i = 0; i < ca.length; i++) {
-			var c = ca[i];
-			while (c.charAt(0) == ' ') {
-				c = c.substring(1, c.length);
+		/* "classic": read and update existing LI from this.elt
+		* @return Switcher
+		*/
+		readItems: function() {
+			var items, i, l, name, title;
+			items = this.elt.getElementsByTagName('LI');
+			l = items.length;
+			if (l === 0) {
+				return this;
 			}
-			if (c.indexOf(nameEQ) == 0) {
-				return c.substring(nameEQ.length, c.length);
-			}
-		}
-		return null;
-	}
 
-	var $ready = function() {
-		var cookie = readCookie("style");
-		var title = cookie ? cookie : getPreferredStyleSheet();
-		console.log('switcher.js', title, cookie);
-		setActiveStyleSheet(title);
-		if (window.jQuery) {
-			jQuery('#styleswitcher')
-				.find('.switcher')
-				.each( function(i, elt) { if( $('#' + $(elt).data().style + '-css').length == 0 ) { $(elt).remove(); } })
-				.end()
-				.live('click', function(evt) { setActiveStyleSheet($(evt.target).data().style); })
-				.toggle();
-		}
-		else if (window.$$) {
-			$$('#styleswitcher .switcher').each(function(elt) {});
-		}
-		else {
-		}
+			for (i=0; i < l; i += 1) {
+				title = items[i].textContent;
+				// sync missing attributes
+				if (!items[i].title) {
+					items[i].title = title;
+				}
+				// quick interface check
+				if (items[i].dataset && items[i].dataset instanceof window.DOMStringMap) {
+					name = items[i].dataset['style'];
+				} else {
+					// read as attribute
+					name = items[i].getAttribute('data-style');
+					// create a simple object for oldIE
+					items[i].dataset = {style:name};
+				}
+
+				if (!name) {
+					name = (items[i].title || title).toLowerCase();
+				}
+				items[i].dataset.style = name;
+				this.styles[name] = title;
+			}
+			return this;
+		},
+
+		/* create LI items in this.elt for each "data-style"
+		* @return Switcher
+		*/
+		createList: function() {
+			var li, list = D.createDocumentFragment();
+			// clear list
+			this.elt.innerHTML = '';
+			// build LI items for .styles
+			for (var prop in this.styles) {
+				if (this.styles.hasOwnProperty(prop)) {
+					prop = prop.toString();
+					if (prop.match(this.exclude)) {
+						continue;
+					}
+					li = D.createElement('li');
+					li.title = this.styles[prop];
+					li.className = this.ITEM_CLASS.replace('{name}', prop);
+					li.innerHTML = this.styles[prop];
+					li['data-style'] = prop;
+					list.appendChild(li);
+				}
+			}
+			this.elt.appendChild(list);
+			return this;
+		},
+		version: 0.1 // float!
 	};
 
-	var $unload = function() {
-		var title = getActiveStyleSheet();
-		createCookie("style", title, 365);
-	};
-
-	if (window.jQuery) {
-		// assign via jQuery
-		jQuery(document).ready($ready);
-		jQuery(document).unload($unload);
-	} else if (window.addEvent) {
-		// assign via Mootools
-		window.addEvent('domready', $ready);
-		window.addEvent('unload', $unload);
+	if ( D.addEventListener ) {
+		D.addEventListener('DOMContentLoaded', Switcher.load, false);
+		W.addEventListener('unload', Switcher.unload, false);
+	} else if ( D.attachEvent ) {
+		D.attachEvent('onreadystatechange', Switcher.load);
+		W.attachEvent('onunload', Switcher.unload);
 	}
 
-})(window, document);
+})(window, window.document);
