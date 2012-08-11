c390ab1fcecafb190d2aa78b79412cb8d9fbc4d2
WebMechanic <serradeil@webmechanic.biz>
2012-08-11 18:05:55 +0200

    + JFormFieldJqueryversion jQuery 1.8.0

--- elements/fields/jqueryversion.php
+++ elements/fields/jqueryversion.php
@@ -20,7 +20,8 @@ class JFormFieldJqueryversion extends JFormFieldList
 	public $type = 'Jqueryversion';
 
 	protected static $versions = array(
-		'1.7.1','1.7.0',
+		'1.8.0',
+		'1.7.2','1.7.1','1.7.0',
 		'1.6.4','1.6.2','1.6.1','1.6.0',
 		'1.5.2','1.5.1','1.5.0',
 		'1.4.4','1.4.3','1.4.2','1.4.1','1.4.0',
