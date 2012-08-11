abfb4e51a6f79e57e432cb5bfa0f34e880698165
WebMechanic <serradeil@webmechanic.biz>
2012-08-11 18:05:15 +0200

    + Feature ElementFeatureMsie metroActivex()

--- elements/features/msie.php
+++ elements/features/msie.php
@@ -6,6 +6,7 @@
  * - browserchoice:  BrowserChoice.com Redirect
  * - cfinstall:  Google Chrome Frame Install for oldIEs
  * - edge:  X-UA-Compatible  !! THIS BETTER GOES INTO .htaccess
+ * - metroActivex:  X-UA-Compatible  !! THIS BETTER GOES INTO .htaccess
  *
  * @package     Construc2
  * @subpackage  Features
@@ -13,6 +14,13 @@
  * @license     GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  */
 
+/**
+ * @link http://blogs.msdn.com/b/ie/  IEBlog
+ * @link http://blogs.msdn.com/b/ieinternals/  IE-Internals
+ * @link http://h5bp.com/i/378  X-UA-Compatible issues
+ * @link http://wiki.whatwg.org/wiki/PragmaExtensions
+ * @link http://blogs.msdn.com/b/ie/archive/2012/01/31/web-sites-and-a-plug-in-free-web.aspx
+ */
 class ElementFeatureMsie extends ElementRendererMeta
 {
 	protected $name = 'msie';
@@ -55,6 +63,7 @@ class ElementFeatureMsie extends ElementRendererMeta
 	 * Google Chrome Frame Install for oldIEs
 	 *
 	 * @return ElementFeatureMsie
+	 * @see edge()
 	 */
 	public function cfinstall($version = '1.0.3')
 	{
@@ -69,23 +78,43 @@ class ElementFeatureMsie extends ElementRendererMeta
 	}
 
 	/**
-	 * Puts IE in standards document mode and trigger Google Chrome frame
+	 * Puts IE7+ in standards document mode and trigger Google Chrome frame
 	 * rendering if installed.
 	 *
 	 * <b>It's highly recommended to add a proper "http-equiv" server
-	 * response header instead</b>, i.e. .htaccess
+	 * response header instead</b>, i.e. .htaccess to prevent the 
+	 * "compatibility view button" to appear.
 	 * <code>Header set X-UA-Compatible "IE=Edge,chrome=1"</code>
 	 *
 	 * @link http://h5bp.com/i/378  X-UA-Compatible issues
 	 * @link http://wiki.whatwg.org/wiki/PragmaExtensions
 	 *
 	 * @return ElementFeatureMsie
+	 * @see metroActivex(), cfinstall()
 	 *
 	 * @todo call ElementRendererHead and "inject" BEFORE data['base']
 	 */
 	public function edge()
 	{
 		return $this->httpEquiv('X-UA-Compatible', 'IE=Edge,chrome=1');
+	}
+
+	/**
+	 * Metro style IE10 detects these flags, and provides a one-touch 
+	 * option to switch to IE10 on the desktop.
+	 *
+	 * <b>IMPORTANT!</b> This currently includes edge() to handle IE8+ ChromeFrame!
+	 * <b>It's highly recommended to add a proper "http-equiv" server response header instead.</b>
+	 *
+	 * <code>Header set X-UA-Compatible "requiresActiveX=true"</code>
+	 * <code><meta http-equiv="X-UA-Compatible" content="requiresActiveX=true" /></code>
+	 *
+	 * @link http://blogs.msdn.com/b/ie/archive/2012/01/31/web-sites-and-a-plug-in-free-web.aspx
+	 * @see edge()
+	 */
+	public function metroActivex()
+	{
+		return $this->httpEquiv('X-UA-Compatible', 'IE=Edge,chrome=1,requiresActiveX=true');
 	}
 
 }
